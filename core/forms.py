3f39f467feb876e8443cbec5cdc48b1c30b3ab8f
mesooneet <mesooneet@gmail.com>
2022-03-11 22:07:44 -0800

    form widgets

--- core/forms.py
+++ core/forms.py
@@ -7,7 +7,11 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name','email','course']
-
+        widgets = {
+            'name':forms.TextInput(attrs={'class':'form-control','id':'nameid'}),
+            'email':forms.EmailInput(attrs={'class':'form-control','id':'emailid'}),
+            'course':forms.TextInput(attrs={'class':'form-control','id':'courseid'})
+        }
 
 
 
